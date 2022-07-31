888338b9ff7d9ecde0c45443bc5873d536ea32a3
adal <adal_vb@outlook.com>
2022-07-31 13:17:41 -0600

    se corrige el formato del documento services

--- src/services.js
+++ src/services.js
@@ -1,12 +1,12 @@
-import bcrypt from "bcrypt"
+const bcrypt = require('bcrypt')
 
 const saltRounds = 10
 
 /* Se crea la clase que tendra las funciones de password */
-export class Password {
+const Password = {
   
   /* Esta funcion sirve para hacer un hash de un password para que despues sea guardada en la base de datos */
-  static async toHash(password) {
+  async toHash(password) {
     const hashedPassword = await new Promise((resolve, reject) => {
       bcrypt.hash(password, saltRounds, ((err, hash) => {
         if (err) reject(err);
@@ -14,10 +14,10 @@ export class Password {
       }))
     })
     return hashedPassword;
-  }
+  },
 
   /* Esta funcion sirve para comparar el hash de un password (storedPassword) y el password que manda el cliente (suppliedPassword) el cual devuelve un true si hicieron match y un false si no fue asi */
-  static async compare(storedPassword, suppliedPassword) {
+  async compare(storedPassword, suppliedPassword) {
     return new Promise((resolve, reject) => {
       bcrypt.compare(suppliedPassword, storedPassword, (err, res) => {
         err ? reject(err) : resolve(res)
