a00591ac50c839e4b11c001e13c2b34b8dc5c519
adal <adal_vb@outlook.com>
2022-07-31 20:02:58 -0600

    m

--- src/services.js
+++ src/services.js
@@ -1,23 +1,27 @@
 const bcrypt = require('bcrypt')
 
 const saltRounds = 10
-  
-/* Esta funcion sirve para hacer un hash de un password para que despues sea guardada en la base de datos */
-export async function ToHash(password) {
-  const hashedPassword = await new Promise((resolve, reject) => {
-    bcrypt.hash(password, saltRounds, ((err, hash) => {
-      if (err) reject(err);
-      resolve(hash)
-    }))
-  })
-  return hashedPassword;
-}
 
-/* Esta funcion sirve para comparar el hash de un password (storedPassword) y el password que manda el cliente (suppliedPassword) el cual devuelve un true si hicieron match y un false si no fue asi */
-export async function ComparePass(storedPassword, suppliedPassword){
-  return new Promise((resolve, reject) => {
-    bcrypt.compare(suppliedPassword, storedPassword, (err, res) => {
-      err ? reject(err) : resolve(res)
+/* Se crea la clase que tendra las funciones de password */
+const Password = {
+  
+  /* Esta funcion sirve para hacer un hash de un password para que despues sea guardada en la base de datos */
+  async toHash(password) {
+    const hashedPassword = await new Promise((resolve, reject) => {
+      bcrypt.hash(password, saltRounds, ((err, hash) => {
+        if (err) reject(err);
+        resolve(hash)
+      }))
     })
-  })
+    return hashedPassword;
+  },
+
+  /* Esta funcion sirve para comparar el hash de un password (storedPassword) y el password que manda el cliente (suppliedPassword) el cual devuelve un true si hicieron match y un false si no fue asi */
+  async compare(storedPassword, suppliedPassword) {
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(suppliedPassword, storedPassword, (err, res) => {
+        err ? reject(err) : resolve(res)
+      })
+    })
+  }
 }
